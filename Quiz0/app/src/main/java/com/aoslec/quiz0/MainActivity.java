96b6eb8fff817f22a4ec29426da9d84186a018ab
hyeeh <hyeji1203@icloud.com>
2021-05-28 16:55:56 +0900

    2021.05.28 Second Commit

--- Quiz0/app/src/main/java/com/aoslec/quiz0/MainActivity.java
+++ Quiz0/app/src/main/java/com/aoslec/quiz0/MainActivity.java
@@ -10,9 +10,10 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     // property
-    Button buttonRed;
-    Button buttonGreen;
-    Button buttonBlue;
+    Button buttonRed, buttonGreen, buttonBlue;
+//    Button buttonRed;
+//    Button buttonGreen;
+//    Button buttonBlue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +24,53 @@ public class MainActivity extends AppCompatActivity {
         buttonGreen = findViewById(R.id.btnGreen);
         buttonBlue = findViewById(R.id.btnBlue);
 
-        buttonRed.setOnClickListener(new View.OnClickListener() { // 버튼레드일 때까지 기다리는 것 -> 부르면 onClick 실
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "빨간색 버튼을 눌렀습니다!", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        buttonRed.setOnClickListener(new View.OnClickListener() { // 버튼레드일 때까지 기다리는 것 -> 부르면 onClick 실행
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "빨간색 버튼을 눌렀습니다!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        buttonGreen.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "초록색 버튼을 눌렀습니다!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        buttonBlue.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "파란색 버튼을 눌렀습니다!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
-        buttonGreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "초록색 버튼을 눌렀습니다!", Toast.LENGTH_SHORT).show();
-            }
-        });
+        // new Ver.
+        buttonRed.setOnClickListener(onClickListener); // 여기는 준비하는 느낌! listener는 준비하는 느낌
+        buttonGreen.setOnClickListener(onClickListener);
+        buttonBlue.setOnClickListener(onClickListener);
 
-        buttonBlue.setOnClickListener(new View.OnClickListener() {
+    } // 처음 킬 때는 이 메소드가 끝임 (onCreate) 딱 한 번만 실행됨 (계속 띄어놓는 상태가 유지됨) 두 번째 페이지는 오버랩 되는 것! 첫 번째 꺼지는 것 아님
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "파란색 버튼을 눌렀습니다!", Toast.LENGTH_SHORT).show();
+                // id 값으로 어떤 값을 눌렀는지 구분하겠다 (v는 View임)
+                String colorString = "";
+                switch (v.getId()){
+                    // R 붙으면 xml 이라고 생각하기! 그래서 R.id.btnRed 임! 뷰에 있는 id를 가져올꺼야! (view는 xml임)
+                    case R.id.btnRed:
+//                        Toast.makeText(MainActivity.this,"빨간색 버튼을 눌렀습니다!",Toast.LENGTH_SHORT).show();
+                        colorString = "빨간색";
+                        break;
+                    case R.id.btnGreen:
+                        colorString = "초록색";
+                        break;
+                    case R.id.btnBlue:
+                        colorString = "파란색";
+                        break;
+                }
+                Toast.makeText(MainActivity.this,colorString + " 버튼을 눌렀습니다!",Toast.LENGTH_SHORT).show();;
             }
-        });
-    }
+        }; // 변수에 함수 기능을 추가한 것이기 때문에 ; 끝에 붙여주어야 함
 }
