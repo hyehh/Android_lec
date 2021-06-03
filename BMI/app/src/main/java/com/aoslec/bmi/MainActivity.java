249ec21e011fc48cc8008f830257a8561dc50eab
hyeeh <hyeji1203@icloud.com>
2021-06-03 18:40:04 +0900

    2021.06.03 Sixth Commit

--- BMI/app/src/main/java/com/aoslec/bmi/MainActivity.java
+++ BMI/app/src/main/java/com/aoslec/bmi/MainActivity.java
@@ -7,12 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout linearFirst, linearSecond, linearThird;
-    Button mainBtn, secondBtn, thirdBtn;
+    LinearLayout linearFirst, linearSecond, linearThird, linearFourth;
+    Button mainBtn, secondBtn, thirdBtn, fourthBtn;
     EditText height, weight;
+
+    double cm, kg, calResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +25,12 @@ public class MainActivity extends AppCompatActivity {
         linearFirst = findViewById(R.id.firstLinear);
         linearSecond = findViewById(R.id.secondLinear);
         linearThird = findViewById(R.id.thirdLinear);
+        linearFourth = findViewById(R.id.fourthLinear);
 
         mainBtn = findViewById(R.id.mainBtn);
         secondBtn = findViewById(R.id.SecondBtn);
         thirdBtn = findViewById(R.id.ThirdBtn);
+        fourthBtn = findViewById(R.id.FourthBtn);
 
         height = findViewById(R.id.editHeight);
         weight = findViewById(R.id.editWeight);
@@ -35,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 linearFirst.setVisibility(v.INVISIBLE);
                 linearSecond.setVisibility(v.VISIBLE);
+                linearThird.setVisibility(v.INVISIBLE);
+                linearFourth.setVisibility(v.INVISIBLE);
             }
         });
 
@@ -44,13 +51,23 @@ public class MainActivity extends AppCompatActivity {
                 linearFirst.setVisibility(v.INVISIBLE);
                 linearSecond.setVisibility(v.INVISIBLE);
                 linearThird.setVisibility(v.VISIBLE);
+                linearFourth.setVisibility(v.INVISIBLE);
             }
         });
 
         thirdBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                linearFirst.setVisibility(v.INVISIBLE);
+                linearSecond.setVisibility(v.INVISIBLE);
+                linearThird.setVisibility(v.INVISIBLE);
+                linearFourth.setVisibility(v.VISIBLE);
 
+                cm = Double.parseDouble(height.getText().toString());
+                kg = Double.parseDouble(weight.getText().toString());
+                calResult = kg / (cm * 0.01 * cm * 0.01);
+                String result = String.format("%.2f", calResult);
+                Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
             }
         });
     }
